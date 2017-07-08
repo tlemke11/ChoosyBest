946aff018e0167c4e60d3eb0e80c4c1aca616fc4
tslemke11 <tyleris@gmail.com>
2017-07-07 22:51:50 -0500

    added basic favorites support and storage (no delete)

---
+++ app/ViewModels/favorites-view-model.js
@@ -0,0 +1,35 @@
+var Observable = require("data/observable").Observable;
+var favoritesViewModel = new Observable();
+var storage = require("../plugins/storage.js");
+
+//Set the defaults
+favoritesViewModel.favList= "";
+
+
+
+favoritesViewModel.loadBookmarks = function(){
+
+    //BLOG Rookie Javascript mistakes -
+    //This use to read
+    // var bookmarks = storage.getBookmarks which assigned bookmarks a property and not the returnvalue of the function
+    var bookmarks = storage.getBookmarks();
+    if (bookmarks == null) {
+        return;
+    }
+
+    bookmarksArray = [];
+
+
+    //don't need to do has own properties check
+    for (var i in bookmarks){
+        if(bookmarks.hasOwnProperty(i)){
+
+            bookmarks[i].asin = i;
+            bookmarksArray.push(bookmarks[i]);
+        }
+    }
+
+    favoritesViewModel.set("favList", bookmarksArray);
+};
+
+exports.favoritesViewModel = favoritesViewModel;
---
+++ app/plugins/storage.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Tyler on 7/6/2017.
+ */
+
+var settingStorage = require("application-settings");
+
+getBookmarks = function() {
+    var bookmarks = settingStorage.getString("bookmarks");
+    if(bookmarks == null) {
+        return {};
+    }
+    return JSON.parse(bookmarks);
+};
+
+setBookmarks = function(bookmarksJSON) {
+    var bookmarksString =  JSON.stringify(bookmarksJSON);
+    settingStorage.setString("bookmarks", bookmarksString);
+};
+
+addToBookmarks = function(asin, name, imgLink){
+    var bookmarks = getBookmarks();
+    if(bookmarks[asin] == null) {
+        bookmarks[asin] = {
+            name: name,
+            image: imgLink
+        };
+        setBookmarks(bookmarks);
+    }
+};
+
+isInBookmarks = function(asin){
+    var bookmarks = getBookmarks();
+    if (bookmarks.hasOwnProperty(asin)) {
+        return true;
+    }
+    return false;
+};
+
+
+removeFromBookmarks = function(asin){
+    if(isInBookmarks(asin)){
+        var bookmarks = getBookmarks();
+        delete bookmarks[asin];
+        setBookmarks(bookmarks);
+    }
+}
+
+exports.getBookmarks = getBookmarks;
+exports.addToBookmarks = addToBookmarks;
+exports.isInBookmarks = isInBookmarks;
+exports.removeFromBookmarks = removeFromBookmarks;
+
